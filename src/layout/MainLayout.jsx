e78aabaf4eee052545c9c7447241206c0c884125
almas hossain <almashossain7384@gmail.com>
2025-02-18 09:53:51 +0600

    dashboard route design ok

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,10 +2,13 @@ import { Outlet } from "react-router-dom"
 
 const MainLayout = () => {
   return (
-    <div>
-    <h1>header page</h1>
+    <div className="flex flex-col md:flex-row justify-center text-center my-6 h-screen">
 
-    <Outlet />
+      <div>
+        <h1>header page</h1>
+
+        <Outlet />
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Link to={'/dashboard'}>
+      <h1 className="text-xl font-bold">Dashboard Route</h1>
+      </Link>
+    </div>
   )
 }
 
